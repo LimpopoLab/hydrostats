b6aab4352658a2d33c76ae517f968a28c3149125
David Kahler <dmkahler@users.noreply.github.com>
2021-08-12 22:04:43 -0400

    Redid inverse Pearson type III according to Kite, 1977.

--- R/pt3.R
+++ R/pt3.R
@@ -1,17 +1,21 @@
 #' Pearson type III distribution
 #' 
-#' This function returns the cumulative distribution function value for 
-#' the Pearson type III distribution, that is, the K value.  It is 
-#' calculated with the gamma function based on the skewness as an input.
+#' This function returns the inverse CDF of the Pearson 
+#' type III distribution, that is, the K value.  It is 
+#' calculated based on the approximation of:
+#' Kite (1977), Frequency and Risk Analysis in Hydrology, 
+#' Water Resouces Publications, Fourt Collins.
+#' as cited in:
+#' Chow, Maidment, and Mays (1988), Applied Hydrology, ch. 12
 #' 
-#' @param m mean of sample
-#' @param s standard deviation of sample
 #' @param c skewness of sample
-#' @return the CDF or K value
+#' @param Fx non-exceedence probability, F(x)=
+#' @return the inverse CDF or K value
 #' @export
-pt3 <- function(m,s,c) {
-      beta <- (2/c)^2
-      lambda <- s/(beta^(0.5))
-      epsilon <- m-(s*(beta^(0.5)))
-      
+pt3 <- function(c,Fx) {
+      k <- c/6
+      z <- pnorminv(Fx)
+      K_T <- z + ((z^2) - 1) * k + ((z^3) -(6*z)) * (k^2) / 3 - ((z^2) - 1) * (k^3) + z * (k^4) + (k^5)/3
+      pt3 <- K_T
+      return(pt3)
 }
