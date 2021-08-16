eacd7c284aa0eb290d1c9ff484352f8ad9d04382
David Kahler <dmkahler@users.noreply.github.com>
2021-08-16 12:59:35 -0400

    Added inverse extreme value distribution type 1.

---
+++ R/evi.R
@@ -0,0 +1,19 @@
+#' Extreme Value type I
+#' 
+#' This function returns the inverse of the Extreme 
+#' Value type I cummulative distribution F(x).  Do 
+#' not normalize these data, EVI uses unique 
+#' geometric statistics.  
+#' 
+#' @param x data
+#' @param Fx Fx non-exceedence probability, F(x)=
+#' @return inverse EVI CDF
+#' @export
+evi <- function(x) {
+      s <- stdev(x)
+      a <- (6^0.5) * s / pi
+      m <- (mean(x, na.rm = TRUE)) - (0.5772 * a)
+      y <- -log(log(1/Fx))
+      evi <- (y * a) + m
+      return(evi)
+}
