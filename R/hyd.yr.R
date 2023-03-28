cf2fd996d47d7751ce78a13e6fe7fed83c8756f5
David Kahler <david.m.kahler@gmail.com>
2023-03-28 17:25:50 -0400

    update start of Southern hemisphere hydro year and added flexible start.

--- R/hyd.yr.R
+++ R/hyd.yr.R
@@ -2,32 +2,38 @@
 #' 
 #' Judges and converts to hydrologic calendar from date.
 #' Note that the hydrologic year starts 01 Oct in the 
-#' Northern hemisphere and 01 Aug in the Southern and 
-#' tropical regions.  Here, the hydrologic year is 
-#' determined by the calendar year in which the 
-#' hydrologic year ends.
+#' Northern hemisphere and 01 Jul in the Southern and 
+#' tropical regions.  Users can also dtermine the start 
+#' month of their custom hydrologic year.  Here, the 
+#' hydrologic year is determined by the calendar year 
+#' in which the hydrologic year ends.
 #' 
 #' @param dt date in year, month, day, POSIX, or lubridate format
-#' @param h "N"orthern or "S"outhern/tropical region
+#' @param s "N"orthern or "S"outhern/tropical region, numerical start month - 01 for January, 12 for December
 #' @return hydrologic year
 #' @export
-hyd.yr <- function(dt, h="N") {
-      if ((h != "N") & (h != "S")) {
+hyd.yr <- function(dt, s="N") {
+      startmonth <- 1
+      if (is.na(s)) {
+            print("Enter a valid location: N or S, or start month numeral")
+      } else if (is.numeric(s)) {
+            startmonth <- s
+      } else if ((s != "N") & (s != "S")) {
             print("Enter a valid location: N or S")
       } else {
             if (h == "N") {
                   startmonth <- 10
             } else if (h == "S") {
-                  startmonth <- 8
+                  startmonth <- 7
             }
-            dt <- as.Date(dt, origin = "1970-01-01")
-            m <- as.numeric(format(dt, "%m"))
-            y <- as.numeric(format(dt, "%Y"))
-            if (m >= startmonth) {
-                  hyd.yr <- y + 1
-            } else {
-                  hyd.yr <- y
-            }
-      return(hyd.yr)
       }
+      dt <- as.Date(dt, origin = "1970-01-01")
+      m <- as.numeric(format(dt, "%m"))
+      y <- as.numeric(format(dt, "%Y"))
+      if (m >= startmonth) {
+            hyd.yr <- y + 1
+      } else {
+            hyd.yr <- y
+      }
+      return(hyd.yr)
 }
